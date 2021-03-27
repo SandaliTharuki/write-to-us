75819643b6e56bac86f3f6ac49f6a1c06fff5c78
User <sandalitharuki95@gmail.com>
2021-03-27 23:28:56 +0530

    unnecessary comment removed in App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Register from './Components/Register';
 function App() {
   return (
     <div className="App">
-      {/* <button type="button" class="btn btn-primary">Register</button>
-      <button type="button" class="btn btn-primary">Log in</button> */}
+      
       <Register/>
     </div>
   );
