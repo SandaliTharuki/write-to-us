dc9d7e70c548f46f893fe6a80709779f8fe31834
User <sandalitharuki95@gmail.com>
2021-04-01 18:28:59 +0530

    labels changed in Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -55,9 +55,9 @@ class Register extends Component{
           <div>
           <form onSubmit={this.handleSubmit}>
             <h1>Register Here!!!</h1>  
-            <label>First Name: </label><input type="text" value={this.state.firstName} onChange={this.firstHandler} /><br />
-            <label>Last Name: </label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /><br />
-            <label>Email: </label><input type="email" value={this.state.email} onChange={this.thirdHandler} /><br />
+            <label>First Name:- </label><input type="text" value={this.state.firstName} onChange={this.firstHandler} /><br />
+            <label>Last Name:- </label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /><br />
+            <label>Email:- </label><input type="email" value={this.state.email} onChange={this.thirdHandler} /><br />
             <label>Password: </label><input type="password" value={this.state.password} onChange={this.passwordHandler} /><br />
             <hr></hr>
             <hr></hr>
