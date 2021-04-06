44d7f8a8e74065d6321bc4544ad419c4353fc829
User <sandalitharuki95@gmail.com>
2021-04-06 21:52:13 +0530

    changed in Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -55,8 +55,8 @@ class Register extends Component{
           <div>
           <form onSubmit={this.handleSubmit}>
             <h1>Register Here!!!</h1>  
-            <label><h4>First Name:- </h4></label><input type="text" value={this.state.firstName} onChange={this.firstHandler} /><br />
-            <label><h4>Last Name:- </h4></label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /><br />
+            <label><h4>First Name </h4></label><input type="text" value={this.state.firstName} onChange={this.firstHandler} /><br />
+            <label><h4>Last Name </h4></label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /><br />
             <label>Email:- </label><input type="email" value={this.state.email} onChange={this.thirdHandler} /><br />
             <label>Password:- </label><input type="password" value={this.state.password} onChange={this.passwordHandler} /><br />
             <hr></hr>
