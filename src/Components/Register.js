3af5f620a333e24f6a3dee3a4d6ba1439c85cb12
User <sandalitharuki95@gmail.com>
2021-05-09 02:21:40 +0530

    hooks added to the function App in Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,67 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 // import Card from '@material-ui/core/Card'
 import './CSS/Register.css'
 import Card from './UIElements/Card'
 
-class Register extends Component{
-    constructor(props){
-        super(props)
+// class Register extends Component{
+//     constructor(props){
+//         super(props)
 
-        this.state = {
-            firstName: "",
-            LastName: "",
-            email: "",
-            password: "",
+//         this.state = {
+//             firstName: "",
+//             LastName: "",
+//             email: "",
+//             password: "",
             
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//         };
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
 
-    firstHandler = (event) => {
-        this.setState({
-            firstName: event.target.value
-          });
-        }
-    secondHandler = (event) => {
-        this.setState({
-            lastName: event.target.value
-            });   
-        }
-    thirdHandler = (event) => {
-        this.setState({
-            email: event.target.value
-                });
-        }
-    passwordHandler = (event) => {
-        this.setState({
-            password: event.target.value
-                    });            
-      } 
+//     firstHandler = (event) => {
+//         this.setState({
+//             firstName: event.target.value
+//           });
+//         }
+//     secondHandler = (event) => {
+//         this.setState({
+//             lastName: event.target.value
+//             });   
+//         }
+//     thirdHandler = (event) => {
+//         this.setState({
+//             email: event.target.value
+//                 });
+//         }
+//     passwordHandler = (event) => {
+//         this.setState({
+//             password: event.target.value
+//                     });            
+//       } 
 
-    handleSubmit(event) {
-        // alert('${this.state.firstName} ${this.state.lastName} Registered successfully')
-        alert(`${this.state.firstName} ${this.state.lastName}  Registered Successfully !!!!`)
-        console.log(this.state);
-        this.setState({
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: "",
-        })
-        event.preventDefault();
-      }  
+//     handleSubmit(event) {
+//         // alert('${this.state.firstName} ${this.state.lastName} Registered successfully')
+//         alert(`${this.state.firstName} ${this.state.lastName}  Registered Successfully !!!!`)
+//         console.log(this.state);
+//         this.setState({
+//             firstName: "",
+//             lastName: "",
+//             email: "",
+//             password: "",
+//         })
+//         event.preventDefault();
+//       }  
 
 
-      render() {
+        function App(){
+
+          const {usernameReg, setUsernameReg} = useState('')
+          const {passwordReg, setPasswordReg} = useState('')
+      
+
+      // render() {
         return (
           <div>
             <Card className="item_content">
           <form onSubmit={this.handleSubmit} className="register-form">
             <h1>Register Here</h1>  
-            <h4><label>First Name </label><input type="text" value={this.state.firstName} onChange={this.firstHandler} /></h4>
+            <h4><label>First Name </label><input type="text" value={this.state.firstName} 
+            onChange={(e) => {
+              setUsernameReg(e.target.value)
+            } }/></h4>
             <h4><label>Last Name </label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /></h4>
             <h4><label>Email </label><input type="email" value={this.state.email} onChange={this.thirdHandler} /></h4>
-            <h4><label>Password </label><input type="password" value={this.state.password} onChange={this.passwordHandler} /></h4>
+            <h4><label>Password </label><input type="password" value={this.state.password} 
+            onChange={(e) => {
+              setPasswordReg(e.target.value);
+            }} /></h4>
             <hr></hr>
             <hr></hr>
             <input type="submit" value="submit" />
@@ -70,7 +82,8 @@ class Register extends Component{
           </div>  
         );
       }
-    }
+    //   }
+    // }
 
 
 
