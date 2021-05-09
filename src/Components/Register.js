3bb17ba7d17b78f6bd1395993e61d779ecb6703e
User <sandalitharuki95@gmail.com>
2021-05-10 00:03:27 +0530

    set values to input variables in Register.js

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react'
 // import Card from '@material-ui/core/Card'
 import './CSS/Register.css'
 import Card from './UIElements/Card'
+import Axios from 'axios'
 
 // class Register extends Component{
 //     constructor(props){
@@ -53,11 +54,24 @@ import Card from './UIElements/Card'
 
 
         function App(){
-
+          const {firstnameReg, setFirstnameReg} = useState('')
+          const {lastnameReg, setLastnameReg} = useState('')
           const {usernameReg, setUsernameReg} = useState('')
+          const {emailReg, setEmailReg} = useState('')
           const {passwordReg, setPasswordReg} = useState('')
       
-
+ 
+        const register = () => {
+          Axios.post("https://localhost3000/register", {
+            firstName: firstnameReg,
+            lastName: lastnameReg,
+            username: usernameReg,
+            email: emailReg,
+            password: passwordReg,
+          }).then((response) =>{
+            console.log(response);
+          });
+        };
       // render() {
         return (
           <div>
@@ -68,8 +82,18 @@ import Card from './UIElements/Card'
             onChange={(e) => {
               setUsernameReg(e.target.value)
             } }/></h4>
-            <h4><label>Last Name </label><input type="text" value={this.state.lastName} onChange={this.secondHandler} /></h4>
-            <h4><label>Email </label><input type="email" value={this.state.email} onChange={this.thirdHandler} /></h4>
+            <h4><label>Last Name </label><input type="text" value={this.state.lastName} 
+            onChange={(e) => {
+              setLastnameReg(e.target.value)
+            }} /></h4>
+            <h4><label>User Name </label><input type="text" value={this.state.lastName} onChange={(e) => {
+              setUsernameReg(e.target.value)
+            } } /></h4>
+            <h4><label>Email </label><input type="email" value={this.state.email} 
+            onChange={(e) => {
+              setEmailReg(e.target.value)
+            }}
+            /></h4>
             <h4><label>Password </label><input type="password" value={this.state.password} 
             onChange={(e) => {
               setPasswordReg(e.target.value);
